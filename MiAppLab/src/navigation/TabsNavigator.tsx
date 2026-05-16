422d6678d2c4352fb8cbb0eb814a865efa56e84d
nivlo28 <efrenhilton1980@gmail.com>
2026-05-15 22:25:52 -0600

    Avance f 2

---
+++ MiAppLab/src/navigation/TabsNavigator.tsx
@@ -0,0 +1,63 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RouteProp } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeTab from '../screens/tabs/HomeTab';
+import IMCTab from '../screens/tabs/IMCTab';
+import ProfileTab from '../screens/tabs/ProfileTab';
+import { RootStackParamList } from './StackNavigator';
+
+export type TabsParamList = {
+  Inicio: { email: string };
+  IMC: undefined;
+  Perfil: { email: string };
+};
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+type Props = {
+  route: RouteProp<RootStackParamList, 'MainTabs'>;
+};
+
+export default function TabsNavigator({ route }: Props) {
+  const { email } = route.params;
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'home';
+
+          if (route.name === 'Inicio') iconName = 'home';
+          if (route.name === 'IMC') iconName = 'calculator';
+          if (route.name === 'Perfil') iconName = 'person';
+
+          return (
+            <Ionicons
+              name={iconName}
+              size={size}
+              color={color}
+            />
+          );
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Inicio"
+        component={HomeTab}
+        initialParams={{ email }}
+      />
+
+      <Tab.Screen
+        name="IMC"
+        component={IMCTab}
+      />
+
+      <Tab.Screen
+        name="Perfil"
+        component={ProfileTab}
+        initialParams={{ email }}
+      />
+    </Tab.Navigator>
+  );
+}
---
+++ MiAppLab/src/screens/LoginScreen.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
+import { RootStackParamList } from '../navigation/StackNavigator';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+export default function LoginScreen({ navigation }: Props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (email && password.length >= 4) {
+      navigation.navigate('MainTabs', { email });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar Sesion</Text>
+
+      <CustomInput
+        type="email"
+        placeholder="Correo"
+        value={email}
+        onChange={setEmail}
+      />
+
+      <CustomInput
+        type="password"
+        placeholder="Contrasena"
+        value={password}
+        onChange={setPassword}
+      />
+
+      <CustomButton
+        title="Ingresar"
+        onPress={handleLogin}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 24,
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 24,
+    color: '#5f0650',
+    textAlign: 'center',
+  },
+});
