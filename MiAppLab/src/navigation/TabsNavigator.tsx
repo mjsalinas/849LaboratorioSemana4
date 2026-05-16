c55d592297aa61e9b8fdee8d14334717ae139b45
Christopher Diaz <70163423+joseblog77@users.noreply.github.com>
2026-05-15 21:14:25 -0600

    Christopher Diaz
    62441274

---
+++ MiAppLab/src/navigation/TabsNavigator.tsx
@@ -0,0 +1,55 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from './StackNavigator';
+import HomeTab from '../screens/tabs/HomeTab';
+import IMCTab from '../screens/tabs/IMCTab';
+import ProfileTab from '../screens/tabs/ProfileTab';
+
+export type TabsParamList = {
+  Inicio: { email: string };
+  IMC: undefined;
+  Perfil: { email: string };
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'MainTabs'>;
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function TabsNavigator({ route }: Props) {
+  const { email } = route.params;
+
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Inicio"
+        component={HomeTab}
+        initialParams={{ email }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="IMC"
+        component={IMCTab}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calculator" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={ProfileTab}
+        initialParams={{ email }}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
