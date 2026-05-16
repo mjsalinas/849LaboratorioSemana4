e625392679c8c57ef21ecea8337e41175fe7f87e
gaby001345-stack <gaby001345@gmail.com>
2026-05-15 23:19:48 -0600

    Actualizando App una vez ya definida las pantallas, componentes, etc.

--- MiAppLab/App.tsx
+++ MiAppLab/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
 import { navigationRef } from './src/navigation/NavigationService';
-
+//Actualización
 export default function App() {
   return (
     <NavigationContainer ref={navigationRef}>
