9536c7fe01d950b3d3a3e1dbe1ad9b3ff424cd2d
carlosfuentes4 <carlosfuent0059@gmail.com>
2026-05-15 22:05:35 -0600

    paso 8 9 y 10

---
+++ MiAppLab/src/navigation/TabsNavigator.tsx
@@ -0,0 +1,40 @@
+// src/navigation/TabsNavigator.tsx
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import HomeTab from "../screens/tabs/HomeTab";
+import IMCTab from "../screens/tabs/IMCTab";
+import ProfileTab from "../screens/tabs/ProfileTab";
+
+// 1. Definimos las pestañas y los parámetros que reciben (Paso 3 de la guía)
+export type TabsParamList = {
+  Inicio: { email: string };
+  IMC: undefined;
+  Perfil: { email: string };
+};
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function TabsNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = "home";
+
+          if (route.name === "Inicio") iconName = "home";
+          else if (route.name === "IMC") iconName = "calculator";
+          else if (route.name === "Perfil") iconName = "person";
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#5f0650",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Inicio" component={HomeTab} />
+      <Tab.Screen name="IMC" component={IMCTab} />
+      <Tab.Screen name="Perfil" component={ProfileTab} />
+    </Tab.Navigator>
+  );
+}
