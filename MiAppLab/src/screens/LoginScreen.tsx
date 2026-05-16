7b3e9a727b47fd8550b61cca02cce30bb0248497
Robert Taylor <robert.taylor.mk@gmail.com>
2026-05-15 22:04:39 -0600

    - Agregando pantalla de Login, por error, la deje vacia y no cargaba la app - Se agrego un titulo de bienvenida y los inputs - se agrego un boton que nos lleva a home - Corregi error en palabra Password

--- MiAppLab/src/screens/LoginScreen.tsx
+++ MiAppLab/src/screens/LoginScreen.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import CustomInput from "../components/CustomInput";
+import CustomButton from "../components/CustomButton";
 
 
 export default function LoginScreen({navigation}:any){
     const [email, setEmail] = useState('');
-    const [password, setPassord] = useState('');
+    const [password, setPassword] = useState('');
 
     const handleLogin = () => {
         if (email && password.length >= 4) {
@@ -11,5 +14,41 @@ export default function LoginScreen({navigation}:any){
     }
     };
 
-    return("");
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Bienvenido a mi App</Text>
+            <CustomInput 
+            placeholder={"Ingresa Correo"} 
+            value={email} 
+            onChange={setEmail}>
+            </CustomInput>
+
+            <CustomInput 
+            placeholder={"Ingresa Contraseña"} 
+            value={password} 
+            onChange={setPassword}>
+            </CustomInput>
+
+            <CustomButton 
+            title={"Ingresar"} 
+            onPress={handleLogin}>
+            </CustomButton>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    backgroundColor: "#ffffff",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 24,
+    textAlign: "center",
+    color: "#000000",
+  },
+});
