3f975093ac1bde2b4be90ba2773402c060eb2da0
Nahun Romero <>
2026-05-15 23:16:34 -0600

    MiAppLab

---
+++ MiAppLab/MiAppLab/src/navigation/TabsNavigator.tsx
@@ -0,0 +1,66 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
+import { RootStackParamList } from "./StackNavigator";
+import HomeTab from "../screens/tabs/HomeTab";
+import IMCTab from "../screens/tabs/IMCTab";
+import ProfileTab from "../screens/tabs/ProfileTab";
+import { RouteProp } from "@react-navigation/native";
+
+export type TabsParamList = {
+  Inicio: { email: string };
+  IMC: undefined;
+  Perfil: { email: string };
+};
+
+type Props = {
+  route: RouteProp<RootStackParamList, "MainTabs">;
+};
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+//3. Utiliza el tab navigator
+export default function TabsNavigator({ route }: Props) {
+  const { email } = route.params;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#ff0000",
+      }}
+    >
+      <Tab.Screen
+        name="Inicio"
+        component={HomeTab}
+        initialParams={{ email }}
+        options={{
+          title: "Mi Inicio",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="IMC"
+        component={IMCTab}
+        options={{
+          title: "Mi IMC",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Perfil"
+        component={ProfileTab}
+        initialParams={{ email }}
+        options={{
+          title: "Mi Perfil",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
---
+++ MiAppLab/MiAppLab/src/screens/LoginScreen.tsx
@@ -0,0 +1,47 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import CustomButton from "../components/CustomButton";
+import CustoInput from "../components/CustomInput";
+import { cache, useState } from "react";
+
+export default function LoginScreen({ navigation }: any) {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  //Funcion para Iniciar Seccion
+  const handleLogin = () => {
+    if (email && password.length >= 4) {
+      navigation.navigate("MainTabs", { email });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <CustoInput
+        type={"email"}
+        placeholder={"Nombre de Usuario"}
+        value={email}
+        onChange={setemail}
+      />
+
+      <CustoInput
+        type={"password"}
+        placeholder={"Ingresa tu Contraseña"}
+        value={password}
+        onChange={setpassword}
+      />
+
+      <CustomButton title={"Iniciar Sesion"} onPress={handleLogin} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
