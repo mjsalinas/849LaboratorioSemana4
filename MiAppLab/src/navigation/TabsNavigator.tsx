20016e459c71910b9c392e681f0594ed513317c8
Angel-Rodas <angel.rodas3060@gmail.com>
2026-05-15 21:21:05 -0500

    tabs navigator inicio imc perfil con email (Classwork)

---
+++ MiAppLab/src/navigation/TabsNavigator.tsx
@@ -0,0 +1,40 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
+import { RootStackParamList } from './StackNavigator';
+import HomeTab from '../screens/tabs/HomeTab';
+import IMCTab from '../screens/tabs/IMCTab';
+import ProfileTab from '../screens/tabs/ProfileTab';
+
+export type TabsParamList = {
+  Inicio: { email: string };
+  IMC: undefined;
+  Perfil: { email: string };
+};
+
+const Tabs = createBottomTabNavigator<TabsParamList>();
+
+type Props = NativeStackScreenProps<RootStackParamList, 'MainTabs'>;
+
+export default function TabsNavigator({ route }: Props) {
+  const { email } = route.params;
+
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: '#5f0650',
+        tabBarIcon: ({ color, size }) => {
+          const icon =
+            route.name === 'Inicio' ? 'home' :
+            route.name === 'IMC' ? 'calculator' : 'person';
+          return <Ionicons name={icon} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tabs.Screen name='Inicio' component={HomeTab} initialParams={{ email }} />
+      <Tabs.Screen name='IMC' component={IMCTab} />
+      <Tabs.Screen name='Perfil' component={ProfileTab} initialParams={{ email }} />
+    </Tabs.Navigator>
+  );
+}
