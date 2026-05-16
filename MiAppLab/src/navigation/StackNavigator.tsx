90beab14da9f4c7626ec8a596439cf878756c312
gaby001345-stack <gaby001345@gmail.com>
2026-05-15 23:18:05 -0600

    Actualizando, y corrigiendo archivos para evitar errores  al mismo tiempo, agregando la pantalla de inicio donde se muestra mi perfil y acceso para cerrar mi sesión.

--- MiAppLab/src/navigation/StackNavigator.tsx
+++ MiAppLab/src/navigation/StackNavigator.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import TabsNavigator from './TabsNavigator';
+
+export type RootStackParamList = {
+  Login: undefined;
+  MainTabs: { email: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 export default function StackNavigator() {
   return (
     <Stack.Navigator 
